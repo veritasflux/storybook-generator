622a86106950a1e0ad5a38705f6b4cb0f81ed472
Amine Brini <60950180+veritasflux@users.noreply.github.com>
2025-01-10 16:52:18 +0100

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -9,7 +9,7 @@ def initialize_image_generator():
     Returns:
         StableDiffusionPipeline: The initialized pipeline.
     """
-    pipeline = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",safety_checker=True)
+    pipeline = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",safety_checker=None)
     pipeline.to("cpu")  # Use CPU
     return pipeline
 
