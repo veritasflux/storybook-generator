408e48651147d4aea8d144ebca7a9a6433beb649
Amine Brini <60950180+veritasflux@users.noreply.github.com>
2025-01-15 15:00:20 +0100

    Update story_generator.py

--- story_generator.py
+++ story_generator.py
@@ -16,11 +16,11 @@ def generate_story(name, animal, adventure):
         str: The generated story.
     """
     # Prepare the story prompt
-    system_prompt =     (    f"Write a children's story (about 250 words) about {name} who embarks on a {adventure} "
-    f"with their favorite animal, a {animal}. The story should have a wise end. For each paragraph, provide a title formatted as follows (Do not add special characters before Title or Content):\n\n
-    Expected output format :
-    "{Title: Title of the Paragraph}\n
-    "{Content: Paragraph text goes here}.\n")
+    system_prompt =     (    f"Write a children's story (about 250 words) about {name} who embarks on a {adventure}"
+    f"with their favorite animal, a {animal}. The story should have a wise end. For each paragraph, provide a title formatted as follows (Do not add special characters before Title or Content):\n\n"
+    f"Expected output format :"
+    f"{Title: Title of the Paragraph}\n"
+    f"{Content: Paragraph text goes here}.\n")
 
     # Call Groq's chat completion
     completion = client.chat.completions.create(
